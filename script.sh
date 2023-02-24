b25f8b9e5263860424b5dcfc9f6836a9743e20a6
徐雨萱 <xuyuxuan@xuyuxuandeMacBook-Pro-2.local>
2023-02-24 16:02:33 +0100

    plz work

--- script.sh
+++ script.sh
@@ -9,20 +9,21 @@ set -e
 # Here is a list of tasks for you.
 
 # 0. Tell me who worked on this together
-echo "student 1"   # please fill in names here
-echo "student 2"
+echo "Yuxuan XU"   # please fill in names here
+echo "Justine NAYRAL"
+echo "Norbert MONTI"
 
 # 1. Go to your home directory: 
 # (enter your command below)
-
+cd
 
 # 2. from your home, creating a directory structure: new folder `scpoprogramming`, and inside that folder create folder `hw1`
 # (enter your command below)
-
+mkdir -p scpoprogramming/hw1
 
 # 3. go into that new directory, i.e. into ~/scpoprogramming/hw1
 # (enter your command below)
-
+cd ~/scpoprogramming/hw1
 
 # 4. download with wget if file does not exist yet
 # if wget does not work for you, manually download from the below URL and place into `~/scpoprogramming/hw1` as `movies.dat`
@@ -33,7 +34,7 @@ if [ ! -f  ~/scpoprogramming/hw1/movies.dat ]; then
     echo "will download now to *current* directory now\n"
     echo ""
     wget https://raw.githubusercontent.com/sidooms/MovieTweetings/44c525d0c766944910686c60697203cda39305d6/snapshots/10K/movies.dat -O ./movies.dat
-fi
+fi                                                      
 
 # check file exists now
 # (don't touch)
@@ -44,7 +45,7 @@ fi
 
 # 5. look at first 4 rows of downloaded data in `movies.dat`
 # (enter your command below)
-
+head -n 4 movies.dat
 
 # actual analysis task: A pipeline
 # we want to know how many genres each movie is classified into
@@ -55,14 +56,13 @@ fi
 #  5 1
 # 10 2
 # meaning we have 2 movies without any genre, 5 movies with 1, 10 with 2, etc
- 
 # I want you to construct a pipeline. let's build it up from the start
 
 # 1. use the `awk` command to separt each row at the `::` delimters
 # fill in for _filename_ the correct file you want to operate on. 
 # then remove the # character from the start of the line and look at the result
 
-# awk -F '::' '{print $3}' _filename_
+awk -F '::' '{print $3}' movies.dat
 
 # 2. observe that the `{print $3}` part prints the third field. 
 # that looks like: genre1|genre2
@@ -72,23 +72,24 @@ fi
 # i.e. it will tell us *how many genres* that movie belonged to. No need to understand the `awk` part.
 # again, remove the # below, fill in for _filename_ and run
 
-# awk -F '::' '{print $3}' _filename_ | awk '{print split($0, a, "\\|")}'
+awk -F '::' '{print $3}' movies.dat | awk '{print split($0, a, "\\|")}'
 
 
 # 3. finish the pipeline by adding 2 commands, exactly like in class, that will produce a contingency table
 # we want to know how many movies belong to 0,1,2,... etc genres. 
 
-# awk -F '::' '{print $3}' _filename_ | awk '{print split($0, a, "\\|")}' | sort | uniq -c
+awk -F '::' '{print $3}' movies.dat | awk '{print split($0, a, "\\|")}' | sort | uniq -c
 
 # 4. redirect (>) the output of your pipeline to a file `outtable.txt` in the current directory
 # (enter your command below: just copy from 3. above and add the redirect)
-
+awk -F '::' '{print $3}' movies.dat | awk '{print split($0, a, "\\|")}' | sort | uniq -c > outtable.txt
+ 
 # 5. print your table to screen
 echo ""   # don't touch
 echo "here is my table:"   # don't touch
 
 # (enter your command below)
-
+cat outtable.txt
 
 #### End of your tasks
 # please do not modify the below lines
